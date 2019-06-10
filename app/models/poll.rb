85dc68f67718ee1607eeb4b3248a46938a15202b
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-06-10 00:05:07 -0400

    DB url change.

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,8 +2,8 @@ require 'json/ext'
 class Poll
 
   # connect to postgres
-  if(ENV['https://arguably-us.herokuapp.com/'])
-    uri = URI.parse(ENV['https://arguably-us.herokuapp.com/'])
+  if(ENV['DATABASE_URL'])
+    uri = URI.parse(ENV['DATABASE_URL'])
     DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
     else
     DB = PG.connect(host: "localhost", port: 5432, dbname: 'arguably_api_development')
